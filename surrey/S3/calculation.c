a6af1a402e56395a70f8268442c560b4e896f1f3
frankcholula <lu.phrank@gmail.com>
2024-10-07 11:33:30 +0100

    finished lab3 and ignore vscode files.

--- surrey/S3/calculation.c
+++ surrey/S3/calculation.c
@@ -2,10 +2,30 @@
 
 #include <stdio.h>  // For printf()
 #include <random.h> // for random_rand()
+#include <math.h>   // for fabs()
 
 /*---------------------------------------------------------------------------*/
 PROCESS(calculation, "Calculation");
 AUTOSTART_PROCESSES(&calculation);
+
+static void print_float(float number){
+    int integer_part = (int)number;
+    int decimal_part = (int)((number - integer_part) * 1000); // Get the two decimal places
+    printf("The square root of the value =: %d.%02d\n", integer_part, decimal_part);
+}
+
+static float babylonian_sqrt(float r) {
+  float x = r;
+  float y = 0;
+  float z = r / 2.0f;  // Initial guess
+  float epsilon = 0.00001f;  // Convergence threshold
+  while (fabs(y - z) >= epsilon) {
+    y = z;
+    z = (x / y + y) / 2;
+  }
+  return y;  // Return the square root as a float
+}
+
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(calculation, ev, data)
 {
@@ -22,8 +42,8 @@ PROCESS_THREAD(calculation, ev, data)
     {
       unsigned short r = random_rand()/4;
       printf("The random value is %d\n", r);
-      printf("The square root of the value = ???\n");
-
+      float sqrt = babylonian_sqrt(r);
+      print_float(sqrt);
       etimer_reset(&timer);
     }
   }
