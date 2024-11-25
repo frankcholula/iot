4e02177fda0185b9b31afb9676c59ca76126d399
Lu <tl01159@heron079.eps.surrey.ac.uk>
2024-11-25 11:09:04 +0000

    fixed destructive bubble sort.

--- surrey/assessment/template-code.c
+++ surrey/assessment/template-code.c
@@ -1,8 +1,8 @@
 #include "contiki.h"
 #include <stdio.h> /* For printf() */
 #include <random.h>  /* For random_rand() */
-
-#define URN  6891988  // specify your URN here
+#include <string.h> /* For memcpy() */
+#define URN  6999999  // specify your URN here
 
 int d1(float f) // Integer part
 {
@@ -37,27 +37,79 @@ void printArray(float *array)
   printf("%d.%d]\n", d1(array[i]), d2(array[i]));
 }
 
-void bubble_sort(float *array, int length) {
+
+
+void bubble_sort(const float *array, float *sorted_array, int length) {
   int i, j;
+  memcpy(sorted_array, array, length * sizeof(float));
   for (i = 0; i < length - 1; i++) {
     for (j = 0; j < length - i - 1; j++) {
-      if (array[j] > array[j + 1]) {
-        float temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
+      if (sorted_array[j] > sorted_array[j + 1]) {
+        float temp = sorted_array[j];
+        sorted_array[j] = sorted_array[j + 1];
+        sorted_array[j + 1] = temp;
       }
     }
   }
 }
 
 float calculate_median(float *array, int length) {
-    bubble_sort(array, length);
+    float sorted_array[length];
+    bubble_sort(array, sorted_array, length);
 
     if (length % 2 == 0) {
-        return (array[length/2 - 1] + array[length/2]) / 2.0;
+        return (sorted_array[length/2 - 1] + sorted_array[length/2]) / 2.0;
     } else {
-        return array[length/2];
+        return sorted_array[length/2];
     }
+}
+
+static float sqrt_approx(float ssd)
+{
+    float error = 0.001; // Error tolerance for Babylonian method
+    float x = ssd;       // Initial guess for square root
+    float difference;
+    int i;
+
+    if (ssd == 0)
+    {
+        return 0.0; // No variance
+    }
+
+    for (i = 0; i < 50; i++)
+    {
+        x = 0.5 * (x + ssd / x);
+        difference = x * x - ssd;
+        if (difference < 0)
+        {
+            difference = -difference;
+        }
+        if (difference < error)
+        {
+            break;
+        }
+    }
+    return x;
+}
+
+
+float calculate_pcc(float *arrayX, float*arrayY, float averageX, float averageY){
+  float sumX = 0;
+  float sumY = 0;
+  float sumXY = 0;
+  float sumX2 = 0;
+  float sumY2 = 0;
+  int i;
+  for (i=0; i<15; i++)
+  {
+    sumX += arrayX[i] - averageX;
+    sumY += arrayY[i] - averageY;
+    sumXY += sumX * sumY;
+    sumX2 += sumX * sumX;
+    sumY2 += sumY * sumY;
+  }
+  float pcc = (sumXY)/ sqrt_approx(sumX2 * sumY2);
+  return pcc;
 }
 
 
@@ -89,7 +141,7 @@ PROCESS_THREAD(sensor_reading_process, ev, data)
   average = average / 15;
   printf("B = ");
   printArray(temp15);
-  printf("Average = %d.%d\n", d1(average), d2(average));
+  printf("Average for B = %d.%d\n", d1(average), d2(average));
 
   // determine the median (implemente your code here)
   float median;
@@ -103,15 +155,19 @@ PROCESS_THREAD(sensor_reading_process, ev, data)
   EMA[0] = temp15[0];
 
   for (i=1; i<15; i++)
-  {
+  { 
     EMA[i] = beta * temp15[i] + (1 - beta) * EMA[i-1];
   }
 
   printf("E = ");
   printArray(EMA);
-
+  float averageE = 0;
+  for (i=0; i<15; i++)
+    averageE = averageE + EMA[i];
+  averageE = averageE / 15;
+  printf("Average for E = %d.%d\n", d1(average), d2(average));
   // determine Pearson Correlation Coefficient (implemente your code here)
-  float pearsonCC = 0;
+  float pearsonCC = calculate_pcc(temp15, EMA, average, averageE);
 
   printf("Pearson Correlation Coefficient = %d.%d\n", d1(pearsonCC), d2(pearsonCC));
 
